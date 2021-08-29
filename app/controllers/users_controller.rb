c95d37d100191f4806692482c66d96340f63b49d
haryu <hryau6@gmail.com>
2021-08-29 12:00:22 +0900

    add: auto_loginを追加し、新規登録後にログインまで済ませるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login!(@user)
       redirect_to login_path, success: 'ようこそ'
     else
       flash.now[:danger] = "もう一度やって"
