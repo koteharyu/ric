cd80b8adadaa6aa3a3d74391194a591a62ce8504
haryu <hryau6@gmail.com>
2021-08-29 11:15:32 +0900

    add: 自動生成されるファイルを制限

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module Ric
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+    end
   end
 end
