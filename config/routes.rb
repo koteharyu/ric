26d7e15f62e95394f1701f3837d19eea966fc1fe
haryu <hryau6@gmail.com>
2021-09-01 16:26:31 +0900

    add: sidekiqの導入

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@
 #        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
 # update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
 #      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -55,5 +56,8 @@ Rails.application.routes.draw do
     resource :account, only: [:edit, :update]
     resources :notifications, only: [:index]
   end
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
 end
