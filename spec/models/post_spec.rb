49f3e4e11bef1377842327eaa36fbf12cf8df4e4
haryu <hryau6@gmail.com>
2021-09-02 12:32:54 +0900

    add: models/post specの雛形作成

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'バリデーション' do
+    it '画像は必須であること' do
+
+    end
+
+    it '本文は必須であること' do
+
+    end
+
+    it '本文は最大1000文字であること' do
+
+    end
+  end
+
+  describe 'スコープ' do
+    describe 'body_contain' do
+
+    end
+    describe 'comment_contain' do
+
+    end
+    describe 'username_contain' do
+
+    end
+  end
+
+end
